23a6ac72ad794052218c705aaf0bc2410dbaed15
mohradza <mioh0887@colorado.edu>
2018-01-09 05:32:25 +0000

    Fixed the FOF code. Now publishes yaw_cmd_msg. Needs to be tested

--- src/FOF_node.cpp
+++ src/FOF_node.cpp
@@ -3,9 +3,11 @@
  */
 
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <math.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <mavros_msgs/CommandBool.h>
@@ -26,14 +28,14 @@ private:
     
     std::vector<float> Qdot_u;
     std::vector<float> Qdot_v;
-    std::vector<float> OF_tang;
-    std::vector<float> OF_tang_prev;
     
     float dt, tau, k_0, alpha, c_psi, c_d;
     float mean_val, mean_sum, std_dev, r_0, d_0;
-    std::vector<float> average_OF_tang;
-    std::vector<float> R_FOF;
-    std::vector<float> LP_OF;
+    float average_OF_tang[];
+    float OF_tang_prev[60];
+    float OF_tang[60]; 
+    float R_FOF[60];
+    float LP_OF[60]; 
     int num_rings; 
     int index; 
     float yaw_rate_cmd;
@@ -46,18 +48,33 @@ private:
 public:
     SubscribeAndPublish(){
         // Create the yaw rate command publisher
-
+        ROS_INFO("Object Created");
         pub_ = nh_.advertise<object_avoidance_cpp::YawRateCmdMsg>
              ("/yaw_cmd_out", 10);
-        // Create flow subscriber     
-        sub_ = nh_.subscribe<object_avoidance_cpp::FlowRingOutMsg>
-             ("/flow_out", 10, &SubscribeAndPublish::flow_cb, this);
 
         //Initialize the gamma array
         for(int i = 0; i < 60; i++){
            gamma_arr[i] = (2*M_PI/60)*i;
+           R_FOF[i] = 0.0;
+           LP_OF[i] = 0.0;
+           OF_tang[i] = 0.0;
+           average_OF_tang[i] = 0.0;
         }
-        
+         
+    }
+    void flow_cb(const object_avoidance_cpp::FlowRingOutMsg::ConstPtr& msg);
+}; // End of class SubscribeAndPublish
+
+void SubscribeAndPublish::flow_cb(const object_avoidance_cpp::FlowRingOutMsg::ConstPtr& msg)
+{
+
+        current_flow = *msg;
+        Qdot_u = current_flow.Qdot_u;
+        Qdot_v = current_flow.Qdot_v;
+        for(int i = 0; i < 60; i++){
+            OF_tang_prev[i] = OF_tang[i];
+        }
+        //ROS_INF0("%f",Qdot_u[1]);
         // Initialize all variables
         dt = .5;
         tau = .75;
@@ -72,15 +89,8 @@ public:
         num_rings = 5;
         index = 0;
         yaw_rate_cmd = 0.0;
-        min_threshold = 0.0; 
-    }
-   
-    void flow_cb(const object_avoidance_cpp::FlowRingOutMsg::ConstPtr& msg){
+        min_threshold = 0.0;
 
-        current_flow = *msg;
-        Qdot_u = current_flow.Qdot_u;
-        Qdot_v = current_flow.Qdot_v;
-        OF_tang_prev = OF_tang;
 
         // Compute average ring flow
         for(int r = 0; r< 5; r++){
@@ -95,8 +105,12 @@ public:
 
         // Reformat the vector to -pi to pi
         for(int i = 0; i < 60; i++){
-            if(i < 30) OF_tang[i] = -average_OF_tang[30 - i];
-            if(i >= 30) OF_tang[i] = average_OF_tang[(90 - 1) - i];
+            if(i < 30){
+                OF_tang[i] = -average_OF_tang[30 - i];
+            }
+            if(i >= 30){
+                OF_tang[i] = average_OF_tang[(90 - 1) - i];
+            }
         }
 
         // Compute the FOF control
@@ -144,20 +158,19 @@ public:
         } else {
             yaw_rate_cmd_msg.yaw_rate_cmd = 0.0;
         }
-    
         pub_.publish(yaw_rate_cmd_msg);    
-    } // End of flow calc
-}; // End of class SubscribeAndPublish
+    // End of flow calc  
+}
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "FOF_node");
+    ros::NodeHandle n;
+    
     SubscribeAndPublish FlowSubPubObject;
-    ros::Time::init();
-    ros::Rate r(150);  // 150 Hz
-    while(ros::ok()){
-        ros::spinOnce();
-        r.sleep();
-    } // End of while loop 
+    
+    ros::Subscriber sub = n.subscribe("/flow_rings",1000, &SubscribeAndPublish::flow_cb, &FlowSubPubObject);
+    
+    ros::spin();
     
     return 0; // Exit main loop     
 }
