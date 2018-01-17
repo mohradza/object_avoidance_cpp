d80f8db8d3ef4f8ba532e637871c6a45d436da24
mohradza <mioh0887@colorado.edu>
2018-01-17 03:15:38 +0000

    Made changes to try to fix non-publishing flow data. Still needs fixed, however it works if FOF_node is run separately of the main launchfile

--- src/FOF.cpp
+++ src/FOF.cpp
@@ -15,16 +15,19 @@
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/RCIn.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 #include <object_avoidance_cpp/YawRateCmdMsg.h>
 #include <object_avoidance_cpp/FlowRingOutMsg.h>
+#include <object_avoidance_cpp/FlowOutMsg.h>
 #include <iostream>
 using namespace std;
 
 class SubscribeAndPublish{
 private:
     ros::NodeHandle nh_;
-    ros::Publisher pub_;
-    ros::Subscriber sub_;
+    ros::Publisher flow_out_pub_;
+    ros::Publisher yaw_cmd_pub_;
+    ros::Subscriber flow_sub;
     
     std::vector<float> Qdot_u;
     std::vector<float> Qdot_v;
@@ -44,13 +47,15 @@ private:
     int sign;
     float gamma_arr[60];
     object_avoidance_cpp::FlowRingOutMsg current_flow;
-
+    object_avoidance_cpp::FlowOutMsg flow_out_msg;
 public:
     SubscribeAndPublish(){
         // Create the yaw rate command publisher
         ROS_INFO("Object Created");
-        pub_ = nh_.advertise<object_avoidance_cpp::YawRateCmdMsg>
+        yaw_cmd_pub_ = nh_.advertise<object_avoidance_cpp::YawRateCmdMsg>
              ("/yaw_cmd_out", 10);
+        flow_out_pub_ = nh_.advertise<object_avoidance_cpp::FlowOutMsg>
+             ("/flow_data_out", 10);
 
         //Initialize the gamma array
         for(int i = 0; i < 60; i++){
@@ -76,11 +81,11 @@ void SubscribeAndPublish::flow_cb(const object_avoidance_cpp::FlowRingOutMsg::Co
         }
         //ROS_INF0("%f",Qdot_u[1]);
         // Initialize all variables
-        dt = .5;
+        dt = .05;
         tau = .75;
         alpha = dt/(tau+dt);
         k_0 = .3;
-        c_psi = -1;
+        c_psi = .1;
         c_d = .25;
         mean_val = 0.0;
         mean_sum = 0.0;
@@ -99,6 +104,8 @@ void SubscribeAndPublish::flow_cb(const object_avoidance_cpp::FlowRingOutMsg::Co
                 average_OF_tang[i] = average_OF_tang[i] + (Qdot_u[index]*sin(gamma_arr[i])-1*Qdot_v[index]*cos(gamma_arr[i]));
             }
         }
+
+//Good        ROS_INFO_THROTTLE(2,"ave_OF_tang: %f", average_OF_tang[45]);
 
         // Compute the spatial average
         for(int i = 0; i < 60; i++) average_OF_tang[i] *= .2; 
@@ -125,6 +132,7 @@ void SubscribeAndPublish::flow_cb(const object_avoidance_cpp::FlowRingOutMsg::Co
         for(int i = 0; i < 59; i++){
             R_FOF[i] = LP_OF[i]*OF_tang[i+1] - OF_tang[i]*LP_OF[i+1];
         }
+//Good        ROS_INFO_THROTTLE(2,"R_FOF: %f", R_FOF[45]);
 
         R_FOF[59] = 0.0;
         mean_sum = 0.0;
@@ -134,12 +142,13 @@ void SubscribeAndPublish::flow_cb(const object_avoidance_cpp::FlowRingOutMsg::Co
             mean_sum += R_FOF[i];
         }
         mean_val = mean_sum / 60;
-
+//Good        ROS_INFO_THROTTLE(2,"Mean Val: %f", mean_val);
         for(int i = 0; i < 60; i++){
             std_dev += pow((R_FOF[i] - mean_val), 2);
         }
         std_dev = pow(std_dev / 60, 0.5);
         min_threshold = 3*std_dev;
+//Good        ROS_INFO_THROTTLE(2,"Min thresh: %f", min_threshold);
 
         // Extract r_0 and d_0 from process OF signal
         for(int i = 0; i < 60; i++){
@@ -158,20 +167,25 @@ void SubscribeAndPublish::flow_cb(const object_avoidance_cpp::FlowRingOutMsg::Co
         } else {
             yaw_rate_cmd_msg.yaw_rate_cmd = 0.0;
         }
-        pub_.publish(yaw_rate_cmd_msg);    
+        yaw_cmd_pub_.publish(yaw_rate_cmd_msg);    
+
+    for(int i = 0; i < 60; i++){
+        flow_out_msg.Qdot_WF_out.push_back(R_FOF[i]);
+    }
+    flow_out_pub_.publish(flow_out_msg);
+    flow_out_msg.Qdot_WF_out.clear();
+
     // End of flow calc  
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "FOF_node");
     ros::NodeHandle n;
-    
     SubscribeAndPublish FlowSubPubObject;
-    
     ros::Subscriber sub = n.subscribe("/flow_rings",1000, &SubscribeAndPublish::flow_cb, &FlowSubPubObject);
-    
-    ros::spin();
-    
+    while(ros::ok()){
+        ros::spin();
+    }
     return 0; // Exit main loop     
 }
 
